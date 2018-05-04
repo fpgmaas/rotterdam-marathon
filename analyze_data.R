25e6f0a10de6692e162353909b2f0a557d8653c5
Unknown <flo12392@gmail.com>
2018-05-04 18:18:00 +0200

    modified width of path

--- analyze_data.R
+++ analyze_data.R
@@ -1,58 +1,51 @@
 ### TODO parse chron to numeric in read_data()
-library(XML)
-library(data.table)
-library(plotly)
 library(ggmap)
 library(ggmapstyles)
+library(data.table)
+library(rjson)
+library(XML)
+library(data.table)
 library(lubridate)
+library(chron)
 
-df_runners = readRDS('data/result.RDS')
+source('funcs.R')
 
-# Read the track data
-doc = xmlParse("data/Marathon Rotterdam 2018.tcx")
-nodes <- getNodeSet(doc, "//ns:Trackpoint", "ns")
-df_track  <- plyr::ldply(nodes, as.data.frame(xmlToList))
-df_track  <- setNames(df_track, c('time', 'lat', 'lon', 'alt', 'distance'))
-df_track$time <- NULL
-for (column in colnames(df_track))
-{
-  df_track[[column]] <- as.numeric(as.character(df_track[[column]]))
-}
-df_track$lon[df_track$distance>39000] = df_track$lon[df_track$distance>39000] + 0.0015
-df_track$lat[df_track$distance>39000] = df_track$lat[df_track$distance>39000] - 0.0009
+df_runners = get_marathon_results()
+df_track = get_track()
+rdam_map <- get_map()
 tot_dist = max(df_track$distance)
-rm(column,doc,nodes)
 
 # Add some noise per runner, so they do not all run on the same straight line.
+set.seed(1)
 df_runners$lat_noise = runif(nrow(df_runners),-0.0006,0.0006)
 df_runners$lon_noise = runif(nrow(df_runners),-0.0006,0.0006)
 
-# Split times
-split_cols = colnames(df_runners)[grepl('_time',colnames(df_runners))]
-df_runners = df_runners[which(rowSums(is.na(df_runners[,split_cols,with=F]))<1),]
-split_times = lapply(1:nrow(df_runners), function(x) {c(0,as.numeric(as.vector(df_runners[x,split_cols,with=F])))*60*24})
-split_dists = as.character(as.vector(df_runners[1,colnames(df_runners)[grepl('split',colnames(df_runners))],with=F]))[c(T,F)]
-split_dists = gsub('k','',split_dists)
-split_dists = c(0,as.numeric(gsub('Finish','42.68',split_dists)))*1000
+# Split times and dists
+X <- get_split_times_per_runner_and_remove_incomplete_runners (df_runners)
+df_runners <- X[['df_runners']]
+split_times <- X[['split_times']]
+split_dists <- get_split_dists(df_runners)
 
-rdam_map = readRDS('data/rdam_map.RDS')
-
-for (minute in 1:10)
+for (minute in 0:360)
 {
-  
+  # Initialize lists to keep track of each runners' lat and lon.
   lon_list = vector('numeric',nrow(df_runners))
   lat_list = vector('numeric',nrow(df_runners))
   
   for(runner in 1:nrow(df_runners))
   {
+    # Find for this runner in which interval he is running, i.e. between which split times.
     interval = findInterval(minute,split_times[[runner]])
-    if(interval<11)
+    if(interval<11) # If the interval<11, he is still running.
     {
-      m_passed = split_dists[interval] + 
-        diff(split_dists[c(interval,interval+1)]) / diff(split_times[[runner]][c(interval,interval+1)]) *
-        (minute-split_times[[runner]][interval])
+      # Find out how many meters this runner has covered by linearly interpolating, starting from his previously known split time.
+      m_already_covered = split_dists[interval] 
+      m_per_minute_this_interval =   diff(split_dists[c(interval,interval+1)]) / diff(split_times[[runner]][c(interval,interval+1)]) 
+      minutes_spent_in_this_interval = minute-split_times[[runner]][interval]
+      m_passed = m_already_covered + m_per_minute_this_interval*minutes_spent_in_this_interval
       
-      point_1 = max(which(m_passed>df_track$distance))
+      # Find out between which two points the runner is running, and interpolate latitude and longitude.
+      point_1 = findInterval(m_passed,df_track$distance)
       point_2 = point_1 + 1
       lon_list[runner] = (df_track$lon[point_1] + df_track$lon[point_2])/2 + df_runners[runner,lon_noise]
       lat_list[runner] = (df_track$lat[point_1] + df_track$lat[point_2])/2 + df_runners[runner,lat_noise]
@@ -64,30 +57,12 @@ for (minute in 1:10)
     }
   }
   
+  # Create data.frame with runners' positions and remove runners that are already finished
   df_position = data.frame(lon=lon_list,lat=lat_list)
-  df_position = df_position[rowSums(is.na(df_position))==0,]
+  df_position = df_position[rowSums(is.na(df_position))==0,] 
   
-  td <- seconds_to_period(minute*60)
-  # rdam_map <- get_map(location = c(4.48,51.90), maptype = "terrain", source = "google", zoom = 12)
-  #> Map from URL : http://maps.googleapis.com/maps/api/staticmap?center=36.971709,-122.080954&zoom=14&size=640x640&scale=2&maptype=terrain&language=en-EN&sensor=false
   png(paste0('images/',minute,'.png'),800,800)
-  print(ggmap(rdam_map,extent = "panel") +
-          geom_path(data = df_track, color='white',alpha=0.9, size = 3, lineend = "round") + 
-          geom_point(data=df_position,aes(x=lon,y=lat),size=1.5,color='#EE7600',alpha=0.7) +
-          theme_void() +
-          theme(plot.title = element_text(hjust = 0.5,
-                                          face = "bold", 
-                                          size = (30),
-                                          margin = margin(t = 20, r = 0, b = 10, l = 0)),
-                plot.subtitle = element_text(hjust = 0.5,
-                                             size = (22),
-                                          margin = margin(t = 10, r = 0, b = 25, l = 0))
-                ) +
-          labs(title='Rotterdam Marathon 2018',subtitle='A timelapse of all runners') +
-          annotate("text", x = 4.45, y = 51.95, label = sprintf('%02d:%02d', td@hour, minute(td)),color='white',size=13) +
-          scale_x_continuous(limits = c(4.466-0.03, 4.546+0.03), expand = c(0, 0)) +
-          scale_y_continuous(limits = c(51.87-0.01, 51.95+0.01), expand = c(0, 0))
-  )
+  print(create_plot(df_position, df_track, minute))
   dev.off()
 }
 
--- funcs.R
+++ funcs.R
@@ -1,27 +1,98 @@
-library(ggmap)
-library(ggmapstyles)
-library(data.table)
-library(rjson)
+# If the file results.RDS does not yet exist, create the file by obtaing results through the API.
+get_marathon_results <- function()
+{
+  if(!file.exists('data/result.RDS'))
+  {
+    source('get_marathon_results_from_api.R')
+  }
+  df_runners <- readRDS('data/result.RDS')
+}
 
-# Read the map
+
+# Read the map from RDS or from Google Maps API.
 get_map <- function()
 {
+  # If RDS with map data already exists, just load RDS.
   if(file.exists('data/rdam_map.RDS'))
   {
     rdam_map <- readRDS('data/rdam_map.RDS')
   }
-  else
+  else # Otherwise, read from API using ggmapstyles and store RDS.
   {
-  rdam_map <- get_snazzymap(center = c(4.48,51.90),
-                            zoom = 12,
-                            mapRef = "https://snazzymaps.com/style/72543/assassins-creed-iv")
-  saveRDS(rdam_map,'data/rdam_map.RDS')
+    register_google_from_json()
+    rdam_map <- get_snazzymap(center = c(4.48,51.90),
+                              zoom = 12,
+                              mapRef = "https://snazzymaps.com/style/72543/assassins-creed-iv")
+    saveRDS(rdam_map,'data/rdam_map.RDS')
   }
   return(rdam_map)
 }
 
+
+# Register google API key from data/key.json
 register_google_from_json <- function()
 {
   register_google(fromJSON(file='data/key.json')$key)
 }
 
+
+# Function that reads the track data from tcx, and returns a data.frame with lon, lat, alt and (cumulative) distance columns.
+get_track <- function()
+{
+  # Read the track data
+  doc = xmlParse("track/Marathon Rotterdam 2018.tcx")
+  nodes <- getNodeSet(doc, "//ns:Trackpoint", "ns")
+  df_track  <- plyr::ldply(nodes, as.data.frame(xmlToList))
+  df_track  <- setNames(df_track, c('time', 'lat', 'lon', 'alt', 'distance'))
+  df_track$time <- NULL
+  for (column in colnames(df_track))
+  {
+    df_track[[column]] <- as.numeric(as.character(df_track[[column]]))
+  }
+  df_track$lon[df_track$distance>39000] = df_track$lon[df_track$distance>39000] + 0.002
+  df_track$lat[df_track$distance>39000] = df_track$lat[df_track$distance>39000] - 0.001
+  df_track$lon[df_track$distance>39000 & df_track$distance<40220] = df_track$lon[df_track$distance>39000 & df_track$distance<40220] - 0.0001
+  df_track$lon[df_track$distance>41400 & df_track$distance<42200] = df_track$lon[df_track$distance>41400 & df_track$distance<42200] - 0.0002
+  df_track$lat[df_track$distance>41400 & df_track$distance<42200] = df_track$lat[df_track$distance>41400 & df_track$distance<42200] + 0.0002
+
+
+  return(df_track)
+}
+
+get_split_times_per_runner_and_remove_incomplete_runners <- function(df_runners){
+  split_cols = colnames(df_runners)[grepl('_time',colnames(df_runners))]
+  df_runners = df_runners[which(rowSums(is.na(df_runners[,split_cols,with=F]))<1),]
+  split_times = lapply(1:nrow(df_runners), function(x) {c(0,as.numeric(as.vector(df_runners[x,split_cols,with=F])))*60*24})
+  return(list('df_runners' = df_runners, 'split_times' = split_times))
+}
+
+get_split_dists <- function(df_runners)
+{
+  split_dists = as.character(as.vector(df_runners[1,colnames(df_runners)[grepl('split',colnames(df_runners))],with=F]))[c(T,F)]
+  split_dists = gsub('k','',split_dists)
+  split_dists = c(0,as.numeric(gsub('Finish','42.68',split_dists)))*1000
+  return(split_dists)
+}
+
+
+create_plot <- function(df_position, df_track, minute)
+{
+  td <- seconds_to_period(minute*60)
+  suppressMessages(p <- ggmap(rdam_map,extent = "panel") +
+    geom_path(data = df_track, color='white',alpha=0.9, size = 4, lineend = "round") + 
+    geom_point(data=df_position,aes(x=lon,y=lat),size=1,color='#EE7600',alpha=0.7) +
+    theme_void() +
+    theme(plot.title = element_text(hjust = 0.5,
+                                    face = "bold", 
+                                    size = (30),
+                                    margin = margin(t = 20, r = 0, b = 10, l = 0)),
+          plot.subtitle = element_text(hjust = 0.5,
+                                       size = (22),
+                                       margin = margin(t = 10, r = 0, b = 25, l = 0))
+    ) +
+    labs(title='Rotterdam Marathon 2018',subtitle='A timelapse of all runners') +
+    annotate("text", x = 4.45, y = 51.95, label = sprintf('%02d:%02d', td@hour, minute(td)),color='white',size=13) +
+    scale_x_continuous(limits = c(4.466-0.03, 4.546+0.03), expand = c(0, 0)) +
+    scale_y_continuous(limits = c(51.87-0.01, 51.95+0.01), expand = c(0, 0)))
+  return(p)
+}
